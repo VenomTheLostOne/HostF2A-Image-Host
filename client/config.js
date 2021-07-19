675f5a51da384fe2cfdcc020aba8f7ca9850170b
Venom <75211518+VenomTheLostOne@users.noreply.github.com>
2021-07-19 11:49:41 +0530

    Add files via upload

---
+++ client/config.js
@@ -0,0 +1,4 @@
+upload.config.server = '' // Empty if the webapp is in the same place as the server
+// upload.config.server = 'https://yourserver.com/' // If the webapp is separated from the server - remember the trailing slash
+upload.config.api_key = 'c61540b5ceecd05092799f936e27755f' // Should be the same as the server, used for uploading
+upload.config.footer = '<a href="/#" target="_blank">Subscribe to my Youtube</a> - <a href="/#" id="contact" target="_blank">https://www.youtube.com/channel/UCDnNlPz2Wa2EW3gi-zOIsPA</a>'
